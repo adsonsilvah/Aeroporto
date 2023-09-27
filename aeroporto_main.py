852bc7fc89f59ff699db869386770f9cd6c9c6cf
Adson Henrique <80604595+adsonsilvah@users.noreply.github.com>
2023-09-26 21:09:13 -0300

    Add files via upload

--- aeroporto_main.py
+++ aeroporto_main.py
@@ -1,31 +1,86 @@
 from escolhas import *
 from CheckIn import *
 from checkout import *
-#parte escolha das cidades
-cidade_origem = ["recife", "garanhuns", "maceio", "natal"]
-#inicializadores 
-escolhas = escolhas(cidade_origem)
+
 checkin = checkin()
 
+print()
+print("Bem vindo(a) ao 321Quilometros")
+print()
 
-print("\nCidades de Origem:\n")
-escolhas.ver_cidade(escolhas.lista_origem)
-origem_cidade = escolhas.escolher_origem()
-
-print("\nCidades de Destino:\n")
-escolhas.ver_cidade(escolhas.lista_chegada)
-destino_cidade = escolhas.escolher_destino()
-
-print(f'Você escolheu {origem_cidade} como sua cidade de partida.\n')
-print(f'\nVocê escolheu {destino_cidade} como sua cidade de chegada.\n')
+cidades = ["Recife", "Garanhuns", "Maceio", "Natal"]
+escolhas = Escolhas(cidades)
+origem = escolhas.escolher_origem()
+print(f"Origem selecionada: {origem}")
+destino = escolhas.escolher_destino()
+print(f"Destino selecionado: {destino}")
 
 
-#fila do checkin
-#checkin.adicionar('adson')
-#checkin.adicionar('alexandre')
-# checkin.adicionar('amanda')
-# checkin.adicionar('elen')
-# checkin.adicionar('eraylson')
+print()
+
+
+print("Escolha a empresa aerea e compre sua passagem:")
+
+
+empresas_aereas = ["Azul", "GOL", "Latam"]
+
+#valores_passagens = {
+ #   "Azul": {"Recife": 300, "Natal": 400, "Garanhuns": 350},
+#   "GOL": {"Recife": 320, "Natal": 410, "Garanhuns": 360},
+#    "Latam": {"Recife": 310, "Natal": 390, "Garanhuns": 340},
+#}
+
+Azul = {"Recife": 300, "Natal": 400, "Garanhuns": 350, "Maceio" : 500}
+Gol = {"Recife": 320, "Natal": 410, "Garanhuns": 360, "Maceio": 510}
+Latam = {"Recife": 310, "Natal": 390, "Garanhuns": 340, "Maceio": 480}
+
+
+aeroportos = cidades
+
+destino_escolhido = destino
+
+chave_selecionada = destino_escolhido
+
+passagens = [("Azul", Azul[destino_escolhido]), ("GOL", Gol[destino_escolhido]), 
+             ("Latam", Latam[destino_escolhido])]
+
+def bubble_sort(passagens):
+    tamanho_passagens = len(passagens)
+    for i in range(tamanho_passagens):
+        for j in range(0, tamanho_passagens - i - 1):
+            if passagens[j][1] > passagens[j + 1][1]:
+                passagens[j], passagens[j + 1] = passagens[j + 1], passagens[j]
+
+bubble_sort(passagens)
+
+print("Empresas disponíveis para o destino", destino_escolhido, ":")
+for empresa in empresas_aereas:
+    print(empresa)
+
+print("Passagens ordenadas por preço:")
+for empresa, preco in passagens:
+    print(f"{empresa}: R${preco:.2f}")
+
+empresa_escolhida = input("Escolha uma empresa aérea: ")
+
+preco_escolhido = None
+
+for empresa, preco in passagens:
+    if empresa == empresa_escolhida:
+        preco_escolhido = preco
+        break
+
+if preco_escolhido is not None:
+    print(f"Você escolheu a empresa aérea: {empresa_escolhida}")
+    print(f"O valor da passagem para {destino_escolhido} é R${preco_escolhido:.2f}")
+else:
+    print("Empresa não encontrada")
+
+print(f'Você escolheu {origem} como sua cidade de partida.\n')
+print(f'Você escolheu {destino} como sua cidade de chegada.\n')
+
+
+
 while True:
     nome = input('nome passageiro: ')
     if nome == "sair":
@@ -36,10 +91,6 @@ Lista_nome_passageiros_saida = checkin.Lista_nome_passageiros
 print(checkin.Lista_nome_passageiros)
 
 checkin.deletar()
-# checkin.deletar()
-# checkin.deletar()
-# checkin.deletar()
-# checkin.deletar()
 
 print(checkin.Lista_nome_passageiros)
 
--- escolhas.py
+++ escolhas.py
@@ -1,4 +1,4 @@
-class escolhas:
+class Escolhas:
     def __init__(self, lista_origem):
         self.lista_origem = lista_origem
         self.lista_chegada = lista_origem.copy()
@@ -8,20 +8,79 @@ class escolhas:
             print(f"{cidade}")
 
     def escolher_origem(self):
+        busca_origem = BuscaBinaria(self.lista_origem)
+
         while True:
             origem = input("Selecione sua cidade de origem: ")
-            if origem in self.lista_origem:
+            if busca_origem.buscar(origem) is not None:
                 self.lista_chegada.remove(origem)
                 return origem
             else:
                 print(f"Cidade {origem} não está na lista de voos. Esta é a lista de nossos voos:")
-                self.ver_cidade(self.lista_origem)
+                lista_ordenada = QuickSort(self.lista_origem)
+                lista_ordenada.sort()
+                self.ver_cidade(lista_ordenada.lista)
 
     def escolher_destino(self):
+        busca_destino = BuscaBinaria(self.lista_chegada)
+
         while True:
             destino = input("Selecione sua cidade de destino: ")
-            if destino in self.lista_chegada:
+            if busca_destino.buscar(destino) is not None: 
                 return destino
             else:
                 print(f"Cidade {destino} não está na lista de voos. Esta é a lista de nossos voos:")
-                self.ver_cidade(self.lista_chegada)
+                lista_ordenada = QuickSort(self.lista_chegada)
+                lista_ordenada.sort()
+                self.ver_cidade(lista_ordenada.lista)
+
+class QuickSort:
+    def __init__(self, lista):
+        self.lista = lista
+
+    def sort(self, inicio=0, fim=None):
+        if fim is None:
+            fim = len(self.lista) - 1
+
+        if inicio < fim:
+            # Particionar
+            divisor = self.particionar(inicio, fim)
+            self.sort(inicio, divisor - 1)
+            self.sort(divisor + 1, fim)
+
+    def particionar(self, inicio, fim):
+        pivot = self.lista[fim]
+        divisor = inicio
+        for i in range(inicio, fim):
+            if self.lista[i] < pivot:
+                self.lista[divisor], self.lista[i] = self.lista[i], self.lista[divisor]
+                divisor += 1
+
+        self.lista[divisor], self.lista[fim] = self.lista[fim], self.lista[divisor]
+        return divisor
+
+class BuscaBinaria:
+    def __init__(self, lista_busca):
+        self.lista_busca = lista_busca
+        self.quick_sort = QuickSort(lista_busca) 
+
+    def buscar(self, alvo):
+        self.quick_sort.sort()  
+        lista_ordenada = self.quick_sort.lista
+
+        esquerda, direita = 0, len(lista_ordenada) - 1
+
+        while esquerda <= direita:
+            meio = (esquerda + direita) // 2
+            elemento_meio = lista_ordenada[meio]
+
+            if elemento_meio == alvo:
+                return meio 
+            elif elemento_meio < alvo:
+                esquerda = meio + 1
+            else:
+                direita = meio - 1
+
+        return None
+
+
